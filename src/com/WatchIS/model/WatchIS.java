c5ec81bd6ff8c6a014b0b9d693f52578799478b7
Santoshi Paudel <sachida@Sachidas-MacBook-Air.local>
2024-12-22 11:54:09 +0545

    fist commit

---
+++ src/com/WatchIS/model/WatchIS.java
@@ -0,0 +1,15 @@
+package com.WatchIS.model;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Santoshi Paudel
+ * LMU ID: 23048656
+ */
+public class WatchIS {
+    
+}
